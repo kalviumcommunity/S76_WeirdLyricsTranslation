6bce143565e52c0f07d057cc3cfab387e41436e3
Nandhini Murugan <nandz1407@gmail.com>
2025-02-06 16:42:26 +0530

    added

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 const dotenv =require("dotenv");
 const connectDatabase = require('./database');
-
 app.get('/ping', (req, res) => {
     res.send('Pong!');
 });
